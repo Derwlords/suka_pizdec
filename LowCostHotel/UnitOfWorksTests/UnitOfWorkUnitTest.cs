0fe0279b6244b6a6a8657433f2022963706c9fdd
Vladislav Dubok <vladislav.dubok@itomi.ch>
2022-06-12 13:10:41 +0300

    unittest

---
+++ LowCostHotel/UnitOfWorksTests/UnitOfWorkUnitTest.cs
@@ -0,0 +1,30 @@
+using LowCostHotel.DataAccessLayer.EF;
+using LowCostHotel.DataAccessLayer.UnitOfWork;
+using Moq;
+using Xunit;
+
+namespace UnitOfWorksTests
+{
+    public class UnitOfWorkUnitTest
+    {
+        [Fact]
+        public void GettingUserRepository()
+        {
+            // arrange
+            
+
+            // assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void GettingHotelRepository()
+        {
+            // arrange
+           
+
+            // assert
+            Assert.True(true);
+        }
+    }
+}
